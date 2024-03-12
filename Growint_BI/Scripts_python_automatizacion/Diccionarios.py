2a10c16b59a383478d0720abe6fa802a3622dc11
gus1972 <91632598+gus1972@users.noreply.github.com>
2024-03-12 14:44:42 +0100

    Add files via upload

--- Growint_BI/Scripts_python_automatizacion/Diccionarios.py
+++ Growint_BI/Scripts_python_automatizacion/Diccionarios.py
@@ -11,11 +11,11 @@ url = 'https://cloud.growintegration.es'
 auth = ('GI_Azure', 'Tinorra1972')
 
 # Ruta del directorio que quieres copiar
-dir_path = '/remote.php/dav/files/GI_Azure/Marcas/TMALL'
+dir_path = '/remote.php/dav/files/GI_Azure/ES%20GD/Marcas/TMALL/'
 print(dir_path)
 
 # Conexión al Blob Storage de Azure
-connection_string = 'DefaultEndpointsProtocol=https;AccountName=dicstoregrow;AccountKey=lTyVa0im3mkn3L6doSOqMwLM+zTna4y2slabXni2NSh+LOH19PCFc96ddtbpqqn9t+xP4acoea0L+ASt2jUyaw==;EndpointSuffix=core.windows.net'
+connection_string = 'DefaultEndpointsProtocol=https;AccountName=dicstoregrow;AccountKey=UpXPqFLwWuruIXBDaIuuH0ypYI/axnzJ5PkCSwp7monNKVyt9d8++B6jqYhc8qgqvqIAWto2zFqC+AStB6s5JQ==;EndpointSuffix=core.windows.net'
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 container_name = 'diccont'
 container_client = blob_service_client.get_container_client(container_name)
@@ -43,11 +43,13 @@ if response.status_code == 207:
             print(file_path)
             # Reemplazar %20 por un espacio en blanco
             file_path = file_path.replace('%20', ' ')
+            print(file_path)
             # Leer el archivo desde Nextcloud
             response = requests.get(url + href, auth=auth)
             print(response)
             if response.status_code == 200:
                 # Comprobar si es un archivo (no termina en '/')
+                print(file_path)
                 if not file_path.endswith('/'):
                     # Subir el archivo a Azure Blob Storage
                     # Solo usar el nombre del archivo, no la estructura de directorios
