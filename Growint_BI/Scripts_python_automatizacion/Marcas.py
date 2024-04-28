c720988b38f4c4e3a1ae6b69e9a727dcecd1a950
gus1972 <91632598+gus1972@users.noreply.github.com>
2024-04-28 21:39:27 +0200

    Update Marcas.py

--- Growint_BI/Scripts_python_automatizacion/Marcas.py
+++ Growint_BI/Scripts_python_automatizacion/Marcas.py
@@ -5,7 +5,7 @@ from unidecode import unidecode
 from urllib.parse import unquote
 import schedule
 import time
-from datetime import datetime
+import datetime
 from azure.core.exceptions import ResourceExistsError
 from concurrent.futures import ThreadPoolExecutor
 
@@ -20,6 +20,7 @@ main_folder = 'Marcas'
 
 # Ruta del directorio que quieres copiar
 dir_path = '/remote.php/dav/files/GI_Azure/ES%20GD/Marcas/'
+print(dir_path)
 
 # Conexión al Blob Storage de Azure
 connection_string = 'DefaultEndpointsProtocol=https;AccountName=storegrowint;AccountKey=ymX1a2/tP2E1gUytPoSoJOjiIJr6S3oEGxA1mxi1B/Ml9lHKoV4pQvaRRbKYv3dhfeNa3BivHIeG+AStiQJOVQ==;EndpointSuffix=core.windows.net'
@@ -27,80 +28,58 @@ blob_service_client = BlobServiceClient.from_connection_string(connection_string
 container_name = 'growint'
 container_client = blob_service_client.get_container_client(container_name)
 
-def job():
-    # Borrar todos los blobs en el contenedor
-    print("Borrando blobs existentes...")
-    blob_list = container_client.list_blobs()
-    for blob in blob_list:
-        print(f"Borrando blob {blob.name}...")
-        blob_client = container_client.get_blob_client(blob.name)
-        blob_client.delete_blob()
+# Borrar todos los blobs en el contenedor
+print("Borrando blobs existentes...")
+blob_list = container_client.list_blobs()
+for blob in blob_list:
+    print(f"Borrando blob {blob.name}...")
+    blob_client = container_client.get_blob_client(blob.name)
+    blob_client.delete_blob()
 
-    # Verificar si todos los blobs se han borrado
-    blob_list = container_client.list_blobs()
-    if len(list(blob_list)) == 0:
-        print("Todos los blobs se han borrado correctamente.")
-    else:
-        print("No todos los blobs se han borrado.")
+# Verificar si todos los blobs se han borrado
+blob_list = container_client.list_blobs()
+if len(list(blob_list)) == 0:
+    print("Todos los blobs se han borrado correctamente.")
+else:
+    print("No todos los blobs se han borrado.")
 
-    # Listar los archivos en el directorio
-    response = requests.request('PROPFIND', url + dir_path, auth=auth, headers={'Depth': 'infinity'})
-    print(response.status_code)
+# Listar los archivos en el directorio
+response = requests.request('PROPFIND', url + dir_path, auth=auth, headers={'Depth': 'infinity'})
+print(response.status_code)
 
-    def upload_file(href):
-        if href != dir_path:  # Ignorar el directorio en sí
-            print(href)
-            print(dir_path)
-            print(len(dir_path))
-            file_path = href[len(dir_path):]  # Quitar la ruta del directorio
-            print(file_path)
-            # Reemplazar %20 por un espacio en blanco
-            file_path = unquote(file_path.replace('%20', ' '))
-            print(file_path)
-            # Leer el archivo desde Nextcloud
-            response = requests.get(url + href, auth=auth)
-            print(response)
-            if response.status_code == 200:
-                today = datetime.datetime.today()
-                if today.weekday() <= 8:  # No es fin de semana
-                    try:
-                        blob_client = container_client.get_blob_client(main_folder + '/' + file_path)
-                        blob_client.upload_blob(response.content)
-                        print("Blob created successfully.")
-                    except ResourceExistsError:
-                        # Handle the case where the blob already exists
-                        print("Blob already exists. You may want to update or delete it.")
-                        blob_client.upload_blob(response.content, overwrite=True)
-            else:
-                print(f'Error al leer el archivo {file_path}: {response.status_code}')
+def upload_file(href):
+    if href != dir_path:  # Ignorar el directorio en sí
+        print(href)
+        print(dir_path)
+        print(len(dir_path))
+        file_path = href[len(dir_path):]  # Quitar la ruta del directorio
+        print(file_path)
+        # Reemplazar %20 por un espacio en blanco
+        file_path = unquote(file_path.replace('%20', ' '))
+        print(file_path)
+        # Leer el archivo desde Nextcloud
+        response = requests.get(url + href, auth=auth)
+        print(response)
+        if response.status_code == 200:
+           # today = datetime.datetime.today()
+            #if today.weekday() < 9:  # No es fin de semana
+                try:
+                    blob_client = container_client.get_blob_client(main_folder + '/' + file_path)
+                    blob_client.upload_blob(response.content)
+                    print("Blob created successfully.")
+                except ResourceExistsError:
+                    # Handle the case where the blob already exists
+                    print("Blob already exists. You may want to update or delete it.")
+                    blob_client.upload_blob(response.content, overwrite=True)
+        else:
+            print(f'Error al leer el archivo {file_path}: {response.status_code}')
 
-    if response.status_code == 207:
-        # Parsear la respuesta XML
-        from xml.etree import ElementTree as ET
-        root = ET.fromstring(response.content)
-        hrefs = [response.find('{DAV:}href').text for response in root.findall('{DAV:}response')]
-        with ThreadPoolExecutor(max_workers=20) as executor:
-            executor.map(upload_file, hrefs)
-    else:
-        print(f'Error al listar el directorio {dir_path}: {response.status_code}')
-
-# Programar el trabajo para que se ejecute de lunes a viernes entre las 07:00 am y las 18:00 pm
-for i in range(8):  # De lunes a viernes
-    schedule.every().day.at("06:54").do(job)
-    schedule.every().day.at("07:54").do(job)
-    schedule.every().day.at("08:54").do(job)
-    schedule.every().day.at("09:54").do(job)
-    schedule.every().day.at("10:54").do(job)
-    schedule.every().day.at("11:54").do(job)
-    schedule.every().day.at("12:54").do(job)
-    schedule.every().day.at("13:54").do(job)
-    schedule.every().day.at("14:54").do(job)
-    schedule.every().day.at("15:54").do(job)
-    schedule.every().day.at("16:54").do(job)
-    schedule.every().day.at("17:54").do(job)
-    schedule.every().day.at("18:54").do(job)
-
-
-while True:
-   schedule.run_pending()
-   time.sleep(1)
+if response.status_code == 207:
+    # Parsear la respuesta XML
+    from xml.etree import ElementTree as ET
+    root = ET.fromstring(response.content)
+    hrefs = [response.find('{DAV:}href').text for response in root.findall('{DAV:}response')]
+    with ThreadPoolExecutor(max_workers=30) as executor:
+        executor.map(upload_file, hrefs)
+else:
+    print(f'Error al listar el directorio {dir_path}: {response.status_code}')
